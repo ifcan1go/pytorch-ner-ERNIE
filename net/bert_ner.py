7afee9cca7eaef396a7e79cf8654f2c9eb75b78e
zyy <youyang87@163.com>
2019-07-05 23:33:16 +0800

    debug_

--- net/bert_ner.py
+++ net/bert_ner.py
@@ -7,6 +7,7 @@ from config import args
 import torch.nn.functional as F
 import torch
 
+
 class Bert_CRF(BertPreTrainedModel):
     def __init__(self,
                  config,
@@ -20,7 +21,7 @@ class Bert_CRF(BertPreTrainedModel):
         self.classifier = nn.Linear(config.hidden_size, num_tag)
         self.apply(self.init_bert_weights)
         self.crf = CRF(num_tag)
-        self.num_tag=num_tag
+        self.num_tag = num_tag
 
     def forward(self,
                 input_ids,
@@ -29,7 +30,7 @@ class Bert_CRF(BertPreTrainedModel):
                 label_id=None,
                 output_all_encoded_layers=False):
         bert_encode, _ = self.bert(input_ids, token_type_ids, attention_mask,
-                                     output_all_encoded_layers=output_all_encoded_layers)
+                                   output_all_encoded_layers=output_all_encoded_layers)
         output = self.classifier(bert_encode)
 
         return output
@@ -38,11 +39,13 @@ class Bert_CRF(BertPreTrainedModel):
         if args.do_CRF:
             loss = self.crf.negative_log_loss(bert_encode, output_mask, tags)
         else:
-            loss_fct = nn.CrossEntropyLoss(ignore_index=0)
-            active_loss = output_mask.view(-1) == 1
-            active_logits = bert_encode.view(-1, self.num_tag)[active_loss]
-            active_labels = tags.view(-1)[active_loss]
-            loss = loss_fct(active_logits, active_labels)
+            loss = torch.autograd.Variable(torch.tensor(0.), requires_grad=True)
+            for ix, (features, tag) in enumerate(zip(bert_encode, tags)):
+                num_valid = torch.sum(output_mask[ix].detach())
+                features = features[output_mask[ix] == 1]
+                tag = tag[:num_valid]
+                loss_fct = nn.CrossEntropyLoss(ignore_index=0)
+                loss = loss + loss_fct(features.view(-1, self.num_tag).cpu(), tag.view(-1).cpu())
         return loss
 
     def predict(self, bert_encode, output_mask):
@@ -51,24 +54,24 @@ class Bert_CRF(BertPreTrainedModel):
             predicts = predicts.view(1, -1).squeeze()
             predicts = predicts[predicts != -1]
         else:
-            logits = F.softmax(bert_encode, dim=2)
-            logits_label = torch.argmax(logits, dim=2)
-            logits_label = logits_label.detach().cpu().numpy()
-            # logits_confidence = [values[label].item() for values, label in zip(logits[0], logits_label[0])]
-            # logits_label = [logits_label[0][index] for index, i in enumerate(output_mask[0]) if i.item() == 1]
-            # logits_label.pop(0)
-            # logits_label.pop()
-            # labels = [args.labels[label] for label in logits_label]
-            # words = word_tokenize(text)
-            predicts=logits_label
+            predicts_ =[]
+            for ix, features, in enumerate(bert_encode):
+                features = features[output_mask[ix] == 1]
+                predict= F.softmax(features,dim=1)
+                predict = torch.argmax(predict,dim=1)
+                predicts_.append(predict)
+            predicts=torch.cat(predicts_,0)
         return predicts
 
     def acc_f1(self, y_pred, y_true):
-        y_pred = y_pred.numpy()
-        y_true = y_true.numpy()
+        try:
+            y_pred = y_pred.numpy()
+            y_true = y_true.numpy()
+        except:
+            pass
         f1 = f1_score(y_true, y_pred, average="macro")
-        correct = np.sum((y_true==y_pred).astype(int))
-        acc = correct/y_pred.shape[0]
+        correct = np.sum((y_true == y_pred).astype(int))
+        acc = correct / y_pred.shape[0]
         return acc, f1
 
     def class_report(self, y_pred, y_true):
@@ -76,6 +79,3 @@ class Bert_CRF(BertPreTrainedModel):
         y_pred = y_pred.numpy()
         classify_report = classification_report(y_true, y_pred)
         print('\n\nclassify_report:\n', classify_report)
-
-
-
