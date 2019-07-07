6cc2544789f914df306edef73850b9cb36f2368b
zyy <youyang87@163.com>
2019-07-07 21:45:56 +0800

    args

--- main/main.py
+++ main/main.py
@@ -5,8 +5,16 @@ import config.args as args
 from util.porgress_util import ProgressBar
 from preprocessing.data_processor import produce_data
 from util.model_util import save_model, load_model
+import argparse
 
 def start():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--do_not_train_ernie", default=False, action='store_true',)
+    parser.add_argument("--do_CRF", default=False, action='store_true', )
+    arg=parser.parse_args()
+    args.do_not_train_ernie=arg.do_not_train_ernie
+    args.do_CRF = arg.do_CRF
+
     produce_data()
     train_iter, num_train_steps = create_batch_iter("train")
     eval_iter = create_batch_iter("dev")
